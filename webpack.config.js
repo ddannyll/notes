2a01a9308dd5611f74b156f5f9a3882b20618fde
ddannyll <daniel.nguyen.s173@gmail.com>
2022-08-28 17:41:53 +1000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+};
